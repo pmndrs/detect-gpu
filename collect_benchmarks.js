45a2a0b8e2bd9f219d85601eaceee87d5ccc5c24
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-07-19 20:31:44 +0200

    benchmarks correctly write

--- collect_benchmarks.js
+++ collect_benchmarks.js
@@ -5,7 +5,7 @@ const util = require('util');
 
 // Vendor
 const fetch = require('node-fetch');
-const { parse } = require('node-html-parser');
+const JSSoup = require('jssoup').default;
 
 // Mobile
 const BENCHMARK_MOBILE_URL = 'https://www.notebookcheck.net/Mobile-Graphics-Cards-Benchmark-List.844.0.html?type=&sort=&professional=2&showClassDescription=1&deskornote=3&archive=1&perfrating=1&or=0&showBars=1&3dmark13_ice_gpu=1&3dmark13_cloud_gpu=1&3dmark13_fire_gpu=1&3dmark11_gpu=1&gpu_fullname=1&architecture=1&pixelshaders=1&vertexshaders=1&corespeed=1&boostspeed=1&memoryspeed=1&memorybus=1&memorytype=1&directx=1';
@@ -13,20 +13,46 @@ const BENCHMARK_MOBILE_URL = 'https://www.notebookcheck.net/Mobile-Graphics-Card
 // Desktop
 const BENCHMARK_DESKTOP_URL = 'https://www.notebookcheck.net/Mobile-Graphics-Cards-Benchmark-List.844.0.html?type=&sort=&showClassDescription=1&deskornote=4&archive=1&perfrating=1&or=0&showBars=1&3dmark13_ice_gpu=1&3dmark13_cloud_gpu=1&3dmark13_fire_gpu=1&3dmark11_gpu=1&gpu_fullname=1&architecture=1&pixelshaders=1&vertexshaders=1&corespeed=1&boostspeed=1&memoryspeed=1&memorybus=1&memorytype=1&directx=1';
 
-function collectBenchmarks() {
-  fetch(BENCHMARK_MOBILE_URL)
-    .then(response => response.text())
-    .then((html) => {
-      const rawTableRows = html.match(/<tr[\s\S]*?<\/tr>/g);
-      rawTableRows.forEach((entry) => {
-        const tableRow = parse(entry);
-        console.log(util.inspect(tableRow.childNodes[0].childNodes, false, null));
-      });
-      //   const tableRows = parse(rawTableRows);
+function collectBenchmark(url) {
+  return new Promise((resolve, reject) => fetch(url)
+      .then(response => response.text())
+      .then((html) => {
+        const soup = new JSSoup(html.replace('<!DOCTYPE html>', ''));
+        const table = soup.find('table');
+        const inputs = table.findAll('input');
 
-      //   console.log(tableRows);
-    })
-    .catch(error => new Error(error));
+        const benchmark = inputs.map((input) => {
+          const score = input.previousElement.text.replace('&nbsp;', '').replace('*', '');
+          let name = '';
+
+          input.previousElement.contents.forEach((row) => {
+            if (row.nextElement.text) {
+              name = row.nextElement.text;
+            }
+          });
+
+          return `${score} - ${name}`;
+        });
+
+        resolve(benchmark);
+      })
+      .catch((error) => {
+        reject(new Error(error.message));
+      }));
 }
 
-collectBenchmarks();
+Promise.all([collectBenchmark(BENCHMARK_DESKTOP_URL), collectBenchmark(BENCHMARK_MOBILE_URL)]).then(
+  (result) => {
+    const file = `
+      export const BENCHMARK_SCORE_DESKTOP = [
+        ${result[0].map(entry => `\n\'${entry}\'`)}
+      ];
+
+      export const BENCHMARK_SCORE_MOBILE = [
+        ${result[1].map(entry => `\n\'${entry}\'`)}
+      ];
+    `;
+
+    console.log(file);
+  },
+);
