44ca1e4225eb29e9acea200c8f5772d9555c83d8
Tim van Scherpenzeel <timvanscherpenzeel@Tims-MacBook-Pro.local>
2018-08-24 17:59:00 +0200

    blacklist

--- src/index.js
+++ src/index.js
@@ -39,6 +39,22 @@ export function getGPUTier(options = {}) {
     renderer = this.forceRendererString;
   }
 
+  // GPU BLACKLIST
+  // - https://wiki.mozilla.org/Blocklisting/Blocked_Graphics_Drivers
+  // - https://www.khronos.org/webgl/wiki/BlacklistsAndWhitelists
+  // - https://chromium.googlesource.com/chromium/src/gpu/+/master/config/software_rendering_list.json
+  const isGPUBlacklisted = /(radeon hd 6970m|radeon hd 6770m|radeon hd 6490m|radeon hd 6630m|radeon hd 6750m|radeon hd 5750|radeon hd 5670|radeon hd 4850|radeon hd 4870|radeon hd 4670|geforce 9400m|geforce 320m|geforce 330m|geforce gt 130|geforce gt 120|geforce gtx 285|geforce 8600|geforce 9600m|geforce 9400m|geforce 8800 gs|geforce 8800 gt|quadro fx 5|quadro fx 4|radeon hd 2600|radeon hd 2400|radeon hd 2600|radeon r9 200|mali-4|mali-3|mali-2)/.test(
+    renderer,
+  );
+
+  if (isGPUBlacklisted) {
+    if (isMobile) {
+      // Return GPU_MOBILE_TIER_0 and mark as blacklisted
+    } else {
+      // Return GPU_DESKTOP_TIER_0 and mark as blacklisted
+    }
+  }
+
   if (isMobile) {
     const mobileBenchmark = getBenchmarkByPercentage(
       BENCHMARK_SCORE_MOBILE,
