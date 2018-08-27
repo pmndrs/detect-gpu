39f558eaf6df4937c45a77a0ac24842c0dfe29b6
Tim van Scherpenzeel <timvanscherpenzeel@Tims-MacBook-Pro.local>
2018-08-27 17:50:39 +0200

    track more kinds of events

--- scripts/analytics_embed.js
+++ scripts/analytics_embed.js
@@ -4,7 +4,7 @@
 
 (function() {
   if (typeof window !== 'undefined' && window && typeof document !== 'undefined' && document) {
-    var trackingCode = 'UA-112999355-2';
+    var trackingCode = 'UA-112999355-3';
 
     (function(i, s, o, g, r, a, m) {
       i['GoogleAnalyticsObject'] = r;
@@ -24,19 +24,75 @@
     $$__analytics('send', 'pageview');
 
     document.addEventListener('DOMContentLoaded', function() {
-      var canvas = document.createElement('canvas');
-      var gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+      // WebGL supported: boolean
+      // Check if WebGL is supported
+      var webglCanvas = document.createElement('canvas');
+      var webgl = webglCanvas.getContext('webgl') || webglCanvas.getContext('experimental-webgl');
 
-      if (gl) {
-        var glExtensionDebugRendererInfo = gl.getExtension('WEBGL_debug_renderer_info');
+      $$__analytics('send', 'event', 'isWebGLSupported', 'load', !!webgl, {
+        nonInteraction: true,
+      });
+
+      // WebGL unmasked renderer: string
+      // Return unmasked renderer string (GPU driver name)
+      if (webgl) {
+        var glExtensionDebugRendererInfo = webgl.getExtension('WEBGL_debug_renderer_info');
         var renderer =
           glExtensionDebugRendererInfo &&
-          gl.getParameter(glExtensionDebugRendererInfo.UNMASKED_RENDERER_WEBGL);
+          webgl.getParameter(glExtensionDebugRendererInfo.UNMASKED_RENDERER_WEBGL);
 
-        $$__analytics('send', 'event', 'WebGL renderer string', 'load', renderer.toString(), {
+        $$__analytics('send', 'event', 'webGLRenderer', 'load', renderer.toString(), {
           nonInteraction: true,
         });
       }
+
+      // WebGL2 support: boolean
+      // Check if WebGL2 is supported
+      var webgl2Canvas = document.createElement('canvas');
+      var webgl2 =
+        webgl2Canvas.getContext('webgl2') || webgl2Canvas.getContext('experimental-webgl2');
+
+      $$__analytics('send', 'event', 'isWebGL2Supported', 'load', !!webgl2, {
+        nonInteraction: true,
+      });
+
+      // Device pixel ratio: number
+      // Ratio of the resolution in physical pixels to the resolution in CSS pixels for the current display device.
+      var devicePixelRatio = window.devicePixelRatio || 1;
+
+      $$__analytics('send', 'event', 'devicePixelRatio', 'load', devicePixelRatio, {
+        nonInteraction: true,
+      });
+
+      // Hardware concurrency: number
+      // Number of logical processors available to run threads on the user's computer
+      var hardwareConcurrency = navigator.hardwareConcurrency || 0;
+
+      $$__analytics('send', 'event', 'hardwareConcurrency', 'load', hardwareConcurrency, {
+        nonInteraction: true,
+      });
+
+      // WebVR support: boolean
+      // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getVRDisplays
+      var isWebVRSupported = !!navigator.getVRDisplays || false;
+
+      $$__analytics('send', 'event', 'isWebVRSupported', 'load', isWebVRSupported, {
+        nonInteraction: true,
+      });
+
+      // WebXR support: boolean
+      // https://immersive-web.github.io/webxr/
+      var isWebXRSupported = !!navigator.xr || false;
+
+      $$__analytics('send', 'event', 'isWebXRSupported', 'load', isWebXRSupported, {
+        nonInteraction: true,
+      });
+
+      // Cleanup
+      webglCanvas = null;
+      webgl2Canvas = null;
+      webgl = null;
+      webgl2 = null;
     });
   }
 })();
