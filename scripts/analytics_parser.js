0dda0b62a6ecff15f51eb5464cf8cb417f41fde3
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-09-03 22:13:52 +0200

    updated renderers, Jest test still broken

--- scripts/analytics_parser.js
+++ scripts/analytics_parser.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 // Vendor
 const csv = require('csvtojson');
+const moment = require('moment');
 
 function getRendererData(type, renderers) {
   const data = renderers.filter(renderer => renderer.field2 === type);
@@ -24,11 +25,14 @@ function parseAnalytics(file) {
       .fromFile(file)
       .then((data) => {
         // Remove static header and footer information
-        const renderers = data.slice(6, data.length - 5);
+        const dateEntry = data.slice(3, 4);
+        const dateRange = dateEntry[0].field1.replace('# ', '').split('-');
+        const timeSpan = moment.duration(moment(dateRange[1]).diff(moment(dateRange[0]))).asDays();
+        const entries = data.slice(6, data.length - (timeSpan + 4)); // Be sure to increase - n with the number of day entries
 
-        const mobileData = getRendererData('mobile', renderers);
-        const tabletData = getRendererData('tablet', renderers);
-        const desktopData = getRendererData('desktop', renderers);
+        const mobileData = getRendererData('mobile', entries);
+        const tabletData = getRendererData('tablet', entries);
+        const desktopData = getRendererData('desktop', entries);
 
         resolve({
           mobileData,
