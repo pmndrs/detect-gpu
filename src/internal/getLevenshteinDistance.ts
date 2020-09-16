0f45633b4ba530afd5c8a9958b8945f374f1cfd8
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-09-17 00:03:19 +0200

    add documentation

--- src/internal/getLevenshteinDistance.ts
+++ src/internal/getLevenshteinDistance.ts
@@ -1,3 +1,5 @@
+// Compute the difference (distance) between two strings
+// SEE: https://en.wikipedia.org/wiki/Levenshtein_distance
 // CREDIT: https://gist.github.com/keesey/e09d0af833476385b9ee13b6d26a2b84
 export const getLevenshteinDistance = (a: string, b: string): number => {
   const an = a ? a.length : 0;
