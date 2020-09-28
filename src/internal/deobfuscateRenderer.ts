4ed1bfc8af21f0d8e1d02d085203d11d8127c507
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-09-28 11:43:16 +0200

    tested iphone 11

--- src/internal/deobfuscateRenderer.ts
+++ src/internal/deobfuscateRenderer.ts
@@ -98,14 +98,12 @@ const deobfuscateAppleGPU = ({
     document.body.appendChild(document.createTextNode(result));
 
     switch (result) {
-      // Unknown:
-      // iPhone 11, 11 Pro, 11 Pro Max (Apple A13 GPU)
-
       case '801621810':
+        // iPhone 11, 11 Pro, 11 Pro Max (Apple A13 GPU)
         // iPad Pro (Apple A12X GPU)
         // iPhone XS, XS Max, XR (Apple A12 GPU)
         // iPhone 8, 8 Plus (Apple A11 GPU)
-        return 'apple a11 gpu';
+        return 'apple a13 gpu';
       case '8016218135':
         // iPhone SE, 6S, 6S Plus (Apple A9 GPU)
         // iPhone 7, 7 Plus (Apple A10 GPU)
