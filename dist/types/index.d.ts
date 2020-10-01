dba382e02f5792d007906df45c36e54c3ee5484e
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-01 16:11:45 +0200

    export types

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,4 +1,16 @@
-import { IGetGPUTier } from './types';
+export interface IGetGPUTier {
+    glContext?: WebGLRenderingContext | WebGL2RenderingContext;
+    mobileBenchmarkPercentages?: number[];
+    desktopBenchmarkPercentages?: number[];
+    failIfMajorPerformanceCaveat?: boolean;
+    forceRendererString?: string;
+    forceMobile?: boolean;
+}
+export declare type TRank = [number, string] | [undefined, undefined];
+export interface IRankWithDistance {
+    rank: TRank;
+    distance: number;
+}
 export declare const getGPUTier: ({ mobileBenchmarkPercentages, desktopBenchmarkPercentages, forceRendererString, forceMobile, glContext, failIfMajorPerformanceCaveat, }?: IGetGPUTier) => {
     tier: string;
     type: string;
