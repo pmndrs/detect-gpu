e14f97b335096aca89105ee95bff0b9cc01d52bc
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-02 15:21:43 +0200

    Update getEntryVersionNumber.ts

--- src/internal/getEntryVersionNumber.ts
+++ src/internal/getEntryVersionNumber.ts
@@ -1,10 +1,13 @@
 export const getEntryVersionNumber = (entryString: string) => {
   entryString = entryString.replace(/\([^\)]+\)/, '');
+  
   let matches = entryString.match(/[\d]+/);
+  
   // If the renderer did not contain any numbers, match letters
   if (!matches) {
     matches = entryString.match(/(\W|^)([a-zA-Z]{1,3})(\W|$)/g);
   }
+  
   // Remove any non word characters and also remove 'amd' which could be matched
   // in the clause above
   return matches ? matches.join('').replace(/\W|amd/g, '') : '';
