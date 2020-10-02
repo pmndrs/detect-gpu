56bc16479ff19949abe325d10f3eaab6577a019f
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-02 15:11:58 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,12 @@ export type TierType =
   | 'BLACKLISTED'
   | 'FALLBACK'
   | 'BENCHMARK';
+
 export type TierResult = {
   tier: number;
   mobile: boolean;
   type: TierType;
   model?: string;
 };
+
 export type ModelEntry = [string, string, number, 0 | 1];
