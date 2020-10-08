21c8ce11de5cd4e3922553e62d8c3a52df6c336c
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-08 22:37:55 +0200

    use filename as key

--- src/full.ts
+++ src/full.ts
@@ -9,8 +9,8 @@ import desktopRadeon from '../benchmarks/d-radeon.json';
 import mobileAdreno from '../benchmarks/m-adreno.json';
 import mobileApple from '../benchmarks/m-apple.json';
 import mobileIntel from '../benchmarks/m-intel.json';
-import mobileMali from '../benchmarks/m-mali.json';
 import mobileMalit from '../benchmarks/m-mali-t.json';
+import mobileMali from '../benchmarks/m-mali.json';
 import mobilePowerVR from '../benchmarks/m-powervr.json';
 
 // Application
@@ -20,19 +20,19 @@ import { getGPUTier as internalGetGPUTier } from '.';
 import { GetGPUTier, ModelEntry } from './types';
 
 const benchmarks = {
-  'd-adreno': desktopAdreno,
-  'd-amd': desktopAmd,
-  'd-apple': desktopApple,
-  'd-geforce': desktopGeforce,
-  'd-intel': desktopIntel,
-  'd-nvidia': desktopNvidia,
-  'd-radeon': desktopRadeon,
-  'm-adreno': mobileAdreno,
-  'm-apple': mobileApple,
-  'm-intel': mobileIntel,
-  'm-mali': mobileMali,
-  'm-mali-t': mobileMalit,
-  'm-powervr': mobilePowerVR,
+  'd-adreno.json': desktopAdreno,
+  'd-amd.json': desktopAmd,
+  'd-apple.json': desktopApple,
+  'd-geforce.json': desktopGeforce,
+  'd-intel.json': desktopIntel,
+  'd-nvidia.json': desktopNvidia,
+  'd-radeon.json': desktopRadeon,
+  'm-adreno.json': mobileAdreno,
+  'm-apple.json': mobileApple,
+  'm-intel.json': mobileIntel,
+  'm-mali-t.json': mobileMalit,
+  'm-mali.json': mobileMali,
+  'm-powervr.json': mobilePowerVR,
 };
 
 export const getGPUTier = (config: GetGPUTier) => {
