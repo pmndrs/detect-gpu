51fbf44c1502034a3981cde69b07789587599d9a
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-14 21:24:29 +0200

    revert example

--- example/index.ts
+++ example/index.ts
@@ -1,15 +1,9 @@
 // Application
 import { getGPUTier } from '../src';
 
-// Data
-import benchmarks from '../benchmarks/all.json';
-
 (async () => {
   const data = await getGPUTier({
-    // benchmarksURL: '/build/benchmarks',
-    override: {
-      loadBenchmarks: async (file: string) => await (benchmarks as any)[file],
-    },
+    benchmarksURL: '/build/benchmarks',
   });
 
   document.body.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
