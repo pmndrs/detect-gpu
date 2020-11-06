2702099b4e42d5cfcbd6a37b00a33a9c026d4d10
puckey <jonathan@puckey.studio>
2020-11-06 14:42:11 +0100

    Improve documentation
    - add docs to GetGPUTier type
    - simplify example
    - move api docs to seperate section (exactly mirroring GetGPUTier type)

--- src/index.ts
+++ src/index.ts
@@ -13,18 +13,58 @@ import { isSSR } from './internal/ssr';
 
 // Types
 export interface GetGPUTier {
+  /**
+   * URL of directory where benchmark data is hosted.
+   *
+   * @default https://unpkg.com/detect-gpu@{version}/dist/benchmarks
+   */
+  benchmarksURL?: string;
+  /**
+   * Optionally pass in a WebGL context to avoid creating a temporary one
+   * internally.
+   */
   glContext?: WebGLRenderingContext | WebGL2RenderingContext;
+  /**
+   * Whether to fail if the system performance is low or if no hardware GPU is
+   * available.
+   *
+   * @default true
+   */
   failIfMajorPerformanceCaveat?: boolean;
+  /**
+   * Framerate per tier for mobile devices.
+   *
+   * @defaultValue [0, 15, 30, 60]
+   */
   mobileTiers?: number[];
+  /**
+   * Framerate per tier for desktop devices.
+   *
+   * @defaultValue [0, 15, 30, 60]
+   */
   desktopTiers?: number[];
+  /**
+   * Optionally override specific parameters. Used mainly for testing.
+   */
   override?: {
     renderer?: string;
+    /**
+     * Override whether device is an iPad.
+     */
     isIpad?: boolean;
+    /**
+     * Override whether device is a mobile device.
+     */
     isMobile?: boolean;
+    /**
+     * Override device screen size.
+     */
     screenSize?: { width: number; height: number };
+    /**
+     * Override how benchmark data is loaded
+     */
     loadBenchmarks?: (file: string) => Promise<ModelEntry[]>;
   };
-  benchmarksURL?: string;
 }
 
 export type TierType =
