758645cf75fcd9c780477b6bc29ac9af6be4c196
Dennis Smolek <desopolis@gmail.com>
2026-01-06 12:39:05 +0900

    Fix Sandbox

--- scripts/update_benchmarks.ts
+++ scripts/update_benchmarks.ts
@@ -184,7 +184,10 @@ type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 });
 
 async function fetchBenchmarks() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
 
   await page.goto(BENCHMARK_URL, { waitUntil: 'networkidle2' });
